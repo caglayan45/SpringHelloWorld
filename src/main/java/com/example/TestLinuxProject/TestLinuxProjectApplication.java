63daa48aa07399da9482002f7e912ea423f3c5d2
Çağlayan Sancaktar <caglayansancaktar@hotmail.com>
2023-02-27 10:17:30 +0300

    initial commit

---
+++ src/main/java/com/example/TestLinuxProject/TestLinuxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestLinuxProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLinuxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLinuxProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestLinuxProject/TestLinuxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestLinuxProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLinuxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
